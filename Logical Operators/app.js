aec5b0ab215c63a04b60136b242db902051f289a
Vanshaj <vanshajsharma2019@gmail.com>
2023-02-28 14:19:23 +0530

    Logical Operators

--- Logical Operators/app.js
+++ Logical Operators/app.js
@@ -5,14 +5,25 @@
 //     console.log("Incorrect format for password")
 // }
 
-const age = 90;
+// const age = 90;
 
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log("Free")
-} else if (age >= 5 && age < 10) {
-    console.log("$10")
-} else if (age >= 10 && age < 65) {
-    console.log("$20")
-} else {
-    console.log("Invalid age")
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("Free")
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10")
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("Invalid age")
+// }
+
+// let firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt('Try again')
+// }
+
+
+const age = 45;
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("You are not a baby or a senior")
 }
