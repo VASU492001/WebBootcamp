8d4f4c0e2d92cfa9858f96bd1631fe3275d19e96
Vanshaj <vanshajsharma2019@gmail.com>
2023-02-28 14:29:22 +0530

    Switch statements

---
+++ Users/app.js
@@ -0,0 +1,42 @@
+const day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+    case 2:
+        console.log("Tuesday");
+    case 3:
+        console.log("Wednesday");
+    case 4:
+        console.log("Thursday");
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+    case 7:
+        console.log("Weekend")
+        break;
+
+    default:
+        console.log("i don't know that")
+
+
+}
+// if (day === 1) {
+//     console.log("Monday")
+// }
+// else if (day === 2) {
+//     console.log("Tuesday")
+// }
+// else if (day === 3) {
+//     console.log("Wednesday")
+// }
+// else if (day === 4) {
+//     console.log("Thursday")
+// }
+// else if (day === 5) {
+//     console.log("Friday")
+// }
+// else {
+//     console.log("I don't know that")
+// }
