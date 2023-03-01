9b8ab7a1989bb13a81a4064db4547dcd9b324d15
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-01 12:45:02 +0530

    WhileLoops

---
+++ WhileLoops/app.js
@@ -0,0 +1,32 @@
+// let count = 0;
+// while (count < 10) {
+//     count++
+//     console.log(count)
+// }
+
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt('enter the secret code...')
+// while (guess !== SECRET) {
+//     guess = prompt("Enter the secret code...")
+// }
+
+// console.log("Congrats you got the secret code")
+
+// let input = prompt("Hey, Say something!")
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === "stop copying me") {
+//         break;
+//     }
+// }
+// console.log("OK You win")
+
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if (i === 100) {
+        break;
+    }
+}
