3a8d28cc95728f5985b23bd1aa2d826ba543b6d0
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-01 11:52:45 +0530

    ForLoops

---
+++ ForLoops/app.js
@@ -0,0 +1,38 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// for (let num = 1; num <= 10; num++) {
+//     console.log("I am in the loop body")
+//     console.log(num);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i)
+// }
+
+// for (let i = 25; i >= 0; i -= 5) {
+//     console.log(i)
+// }
+
+
+
+// IT IS AN INFINITE LOOP  DO NOT RUN IT
+
+// for (let i = 20; i >= 0; i--) {
+//     console.log(i)
+// }
+
+///!!!!!!
