084e6f860e45095d8216590cbbafc73c22dfd9bd
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-02 15:14:07 +0530

    TryCatch

---
+++ Try/Catch/app.js
@@ -0,0 +1,20 @@
+
+
+// try {
+//     hello.toUpperCase();
+// } catch {
+//     console.log("Error!!!")
+// }
+
+// hello.toUpperCase();
+
+// console.log("After!")
+
+function yell(msg) {
+    try {
+        console.log(msg.toUpperCase().repeat(3));
+    } catch (e) {
+        console.log(e);
+        console.log("Please pass a string next time!!")
+    }
+}
