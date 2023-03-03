32f293ec7a1b5ea446c23038a5a0332431e89ad8
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-03 15:31:08 +0530

    Default

---
+++ DefaultParams/app.js
@@ -0,0 +1,13 @@
+// function rollDie(numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+function rollDie(numSides = 6) {
+    return Math.floor(Math.random() * numSides) + 1
+}
+
+function greet(person, msg = "Hey There!!", punc = '!') {
+    console.log(`${msg}, ${person}`)
+}
