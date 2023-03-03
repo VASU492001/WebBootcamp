b75d05b5c3c7469cb927363d1f5a65d2033e5306
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-03 16:06:14 +0530

    Arrays

---
+++ DestructuringArrays/app.js
@@ -0,0 +1,6 @@
+const scores = [929321, 4568787, 665468, 789431, 456712, 1230310, 1234454]
+
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...everyoneElse] = scores;
