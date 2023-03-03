e8d0e7a35fb57d64e5f81be499e5114e43ebf404
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-03 13:57:11 +0530

    SomeEvery

---
+++ SomeEvery/app.js
@@ -0,0 +1,55 @@
+const exams = [80, 98, 92, 78, 70, 90, 89, 84, 81, 77]
+
+exams.some(score => score >= 75)
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        ýear: 2013
+    },
+    {
+        title: '13 Going on 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle all the way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+
+movies.some(movie => movie.year > 2015)
