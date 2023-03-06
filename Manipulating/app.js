2ca5f5b520520fc0520365fc1e136f6358ab297a
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-06 13:15:12 +0530

    DOM

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -12,8 +12,22 @@
 // }
 
 
-const links = document.querySelectorAll('p a');
+// const links = document.querySelectorAll('p a');
 
-for (let link of links) {
-    console.log(link.href)
+// for (let link of links) {
+//     console.log(link.href)
+// }
+
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!'
+// }
+
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy';
 }
