e59be44a0463bdbc09188ceae39129dd7e90180e
Vanshaj <vanshajsharma2019@gmail.com>
2023-03-11 12:08:17 +0530

    Form

--- PreventDefault/app.js
+++ PreventDefault/app.js
@@ -1,13 +1,26 @@
-const form = document.querySelector("#shelterForm");
-const input = document.querySelector("#catName");
-const list = document.querySelector("#dogs")
-form.addEventListener("submit", function (e) {
+const tweetForm = document.querySelector("#tweetForm");
+const tweetsContainer = document.querySelector("#tweets")
+tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(input.value);
-    const catName = input.value;
-    const newLI = document.createElement("LI");
-    newLI.innerText = catName;
-    console.log(newLI);
-    list.append(newLI);
-    input.value = "";
-});
+    // const usernameInput = document.querySelectorAll('input')[0];
+    // const tweetInput = document.querySelectorAll('input')[1];
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = '';
+    tweetInput.value = '';
+
+
+
+})
+
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)
+    newTweet.append(bTag);
+    newTweet.append(`-${tweet}`)
+    tweetsContainer.append(newTweet);
+
+}
